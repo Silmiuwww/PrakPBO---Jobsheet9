87895ace214ecf8b8bf6c0ffa53546347e67dabf
Silmiuww <nadiasalma078@gmail.com>
2025-11-06 22:55:56 +0700

    Langkah 8

--- InterfaceLatihan/SmartFridge.java
+++ InterfaceLatihan/SmartFridge.java
@@ -1,10 +1,27 @@
 package InterfaceLatihan;
-
-public class SmartFridge extends Kulkas {
+public class SmartFridge extends Kulkas implements Audible {
     private int volume;
 
     public SmartFridge(int jumlahPintu, double harga, String warna, String merk, int volume) {
         super(jumlahPintu, harga, warna, merk);
+        this.volume = volume;
+    }
+
+    @Override
+    public void naikkanVolume(int increment) {
+        volume += increment;
+    }
+
+    @Override
+    public void turunkanVolume(int decrement) {
+        volume -= decrement;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
         this.volume = volume;
     }
 }
